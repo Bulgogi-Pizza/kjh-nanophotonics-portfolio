c4cf70a6774f71d3e09ea20498e4f602d07d9a42
Bulgogi-Pizza <quitendexit@gmail.com>
2025-08-13 19:10:26 +0900

    feat: 블루-그린 방식의 무중단 배포 파이프라인 구축 - Nginx가 upstream 변수를 사용하도록 설정을 변경함. - docker-compose.yml에 app_blue와 app_green 서비스를 분리하여 정의함. - 배포 로직을 담당하는 deploy.sh 스크립트를 작성하여,   새 버전 컨테이너 실행 -> 헬스 체크(대기) -> Nginx 트래픽 전환 -> 구 버전 컨테이너 중지   순서로 무중단 배포를 수행하도록 함. - GitHub Actions 워크플로우는 deploy.sh 스크립트를 실행하도록 단순화함.

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+RUNNING_CONTAINER=$(docker ps --filter "name=app_" --format "{{.Names}}")
+
+if [ "$RUNNING_CONTAINER" == "app_blue" ]; then
+  CURRENT_COLOR="blue"
+  NEXT_COLOR="green"
+else
+  CURRENT_COLOR="green"
+  NEXT_COLOR="blue"
+fi
+
+echo "### CURRENTLY RUNNING: $CURRENT_COLOR"
+echo "### STARTING DEPLOYMENT FOR: $NEXT_COLOR"
+
+docker compose build app_${NEXT_COLOR}
+docker compose up -d app_${NEXT_COLOR}
+
+echo "### WAITING FOR app_${NEXT_COLOR} to be health..."
+sleep 10
+# 임시로 10초 기다리는 방편, 추후에는 healthy API 호출하여 200 OK 응답 확인하기
+
+echo "### SWITCHING NGINX to $NEXT_COLOR"
+echo "set \$upstream app_${NEXT_COLOR}:3000;" > ./nginx/conf.d/upstream.conf
+docker compose exec nginx nginx -s reload
+
+echo "### STOPPING old container: app_${CURRENT_COLOR}"
+docker compose stop app_${CURRENT_COLOR}
+docker compose rm -f app_${CURRENT_COLOR}
+
+echo "### DEPLOYMENT COMPLETED: $NEXT_COLOR is now live."
